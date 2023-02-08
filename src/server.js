60a90e9bdf34d743cf67039b845fd12e856d2076
Leonardorossato <leonardorossato21@gmail.com>
2023-02-07 21:17:05 -0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -4,8 +4,9 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
 
-app.listen(mongoConnection)
+app.use(express.json());
 
+app.listen(mongoConnection);
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
