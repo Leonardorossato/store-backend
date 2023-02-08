92bd9a53e2636b1f6521fc34a8004ef53929a087
Leonardorossato <leonardorossato21@gmail.com>
2023-02-07 21:09:07 -0300

    feat: create server.js

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT;
+const mongoConnection = require("./config/mongo.config");
+
+app.mongoConnection = mongoConnection;
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
