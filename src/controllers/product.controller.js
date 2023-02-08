5c630eda89c5f7a534fbc387051d7c0c8c7a970a
Leonardorossato <leonardorossato21@gmail.com>
2023-02-07 21:21:08 -0300

    feat: create product.controller.js

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,12 @@
+class ProductController {
+  static findAll = async (req, res) => {};
+
+  static create = async (req, res) => {};
+
+  static findOne = async (req, res) => {};
+
+  static update = async (req, res) => {};
+
+  static delete = async (req, res) => {};
+}
+module.exports = ProductController;
