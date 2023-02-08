455442acf03d779edb1cda6115d1def462ea162c
Leonardorossato <leonardorossato21@gmail.com>
2023-02-08 09:35:58 -0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -3,8 +3,11 @@ const mongoConnection = require("./config/mongo.config");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
+const productRouter = require("./routes/product.routes");
 
 app.use(express.json());
+
+app.use("/api/products", productRouter);
 
 app.listen(mongoConnection);
 
